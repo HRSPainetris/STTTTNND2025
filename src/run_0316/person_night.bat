0a2858a1ee0906982513b5b5d139fdb1dc708095
thaihienbui <thaihien091009@gmail.com>
2025-03-16 19:19:25 +0700

    Update .bat files for run

---
+++ src/run_0316/person_night.bat
@@ -0,0 +1,4 @@
+@echo off
+cd src
+start "" cmd /k "mode con: cols=80 lines=15 && title Hai Trinh && conda activate khkt2025 && cd .. && python 0316_person_night.py"
+start "" cmd /k "mode con: cols=80 lines=15 && title Drowsiness && conda activate khkt2025 && cd .. && python 0316_drowsiness.py"
--- src/win_run_ship_day.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd src
-start "" cmd /k "mode con: cols=80 lines=15 && title Hai Trinh && conda activate khkt2025 && python 0316_ship_day.py"
-start "" cmd /k "mode con: cols=80 lines=15 && title Drowsiness && conda activate khkt2025 && python 0316_drowsiness.py"
